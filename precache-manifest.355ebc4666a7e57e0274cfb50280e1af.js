055e2d8b3c54cc497aa503c6ab542c19b1d1d964
Ali Can İpek <alicanipek@outlook.com.tr>
2020-04-21 23:15:33 +0300

    Updates

---
+++ precache-manifest.355ebc4666a7e57e0274cfb50280e1af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385aeabf40a48c9ea559604b97981816",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "40cbf547323a68aa9ea0",
+    "url": "/game-of-life/static/css/main.e7b0b536.chunk.css"
+  },
+  {
+    "revision": "3117f0415d4bc194ac1b",
+    "url": "/game-of-life/static/js/2.bf650fca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.bf650fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cbf547323a68aa9ea0",
+    "url": "/game-of-life/static/js/main.07125d2c.chunk.js"
+  },
+  {
+    "revision": "39f8af036aee07664d58",
+    "url": "/game-of-life/static/js/runtime-main.1c12863f.js"
+  }
+]);
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Game from "./Components/Game";
-
-function App() {
-    return <Game row={43} col={92} />;
-}
-
-export default App;
