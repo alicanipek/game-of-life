76b55bbf9cc86dbac370a0c3fccc5624259f5028
Ali Can İpek <alicanipek@outlook.com.tr>
2020-10-17 13:01:55 +0300

    Grid size made changeable. Animation stop added after final state of grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import Game from "./Components/Game";
+import React, { useState } from 'react';
+import Game from './Components/Game';
 
 function App() {
-    return <Game row={43} col={92} />;
+    const [row, setRow] = useState(0);
+    const [col, setCol] = useState(0);
+
+    return (
+        <>
+            <input
+                type="number"
+                onChange={(e) => setRow(Number(e.currentTarget.value))}
+            />
+            <input
+                type="number"
+                onChange={(e) => setCol(Number(e.currentTarget.value))}
+            />
+            <Game row={row} col={col} />
+        </>
+    );
 }
 
 export default App;
